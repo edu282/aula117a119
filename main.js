e6dc260a3cd6e8608fa98d303cfe65f26c41d5e6
edu282 <91227097+edu282@users.noreply.github.com>
2023-08-29 16:24:12 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,50 @@
 function setup()
 {
     canvas = createCanvas(280, 280);
-    canvas.center();
+    canvas.position(627, 250);
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+}
+
+function preload()
+{
+    classifier = ml5.imageClassifier('DoodleNet');
+}
+
+function clearCanvas()
+{
     background("white");
 }
 
 function draw()
 {
-    canvas(10,10,280,280);
+ strokeWeight(10);
+ stroke(0);
+ if (mouseIsPressed)
+ {
+    line(pmouseX, pmouseY, mouseX, mouseY);
+ }
+}
+
+
+function classifyCanvas()
+{
+    classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if (error)
+    {
+        console.error(error);
+    }
+    console.log(results);
+    var result = resutls[0].label;
+    document.getElementById('label').innerHTML = 'Nome: ' + result.replace('_', ' ');
+
+    document.getElementById('confidence').innerHTML = 'Precisão: ' + Math.round(results[0].confidence * 100) + '%';
+
+    utterThis = new SpeechSynthesisUtterance(result.replace ('_', ' '));
+    synth.speak(utterThis);
 }
